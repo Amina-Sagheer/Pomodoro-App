51ef3a6c9fe6ee2b0e47ae804918dadb2031d57b
Amina-Sagheer <sagheeramina32@gmail.com>
2024-06-29 18:21:26 -0600

    Add start button functionality

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from tkinter import *
 
+i = 0
+
+
 def count_down(count):
-    minutes = count // 60
-    seconds = count % 60
-    final_count = f"{minutes}:{seconds}"
-    canvas.itemconfig(timer, text=final_count)
-    window.after(1000, count_down, count - 1)
+    if count >= 0:
+        minutes = count // 60
+        seconds = count % 60
+        final_count = f"{minutes:02}:{seconds:02}"
+        canvas.itemconfig(timer, text=final_count)
+        window.after(1000, count_down, count - 1)
+
 
 def start_timer():
-    count_down(5 * 60)
+    def start_work():
+        global i
+        i += 1
+        count_down(25 * 60)  # 25 minutes of work
+        window.after(25 * 60 * 1000, start_break)
 
+    def last_session():
+        count_down(20 * 60)  # 25 minutes of work
+
+    def start_break():
+        count_down(5 * 60)  # 5 minutes of short break
+        window.after(5 * 60 * 1000, start_work)
+
+    # Start the first work session
+    if i == 4:
+        last_session()
+    else:
+        start_work()
 
 
 window = Tk()
@@ -19,22 +40,19 @@ window.config(padx=10, pady=10, bg="#FFE8C5")
 label = Label(text="Timer", fg='green', bg="#FFE8C5", font=("Arial", 16, "bold"))
 label.grid(column=2, row=1)
 
-start_button = Button(text="Start")
-start_button.grid(column=1, row=3)
-#
-# stop_button = Button(text="Stop")
-# stop_button.grid(column=3, row=3)
-
 canvas = Canvas(width=400, height=300, bg="#FFE8C5", highlightthickness=0)
 image_url = PhotoImage(file="tomato.png")
 canvas.create_image(200, 150, image=image_url)
 timer = canvas.create_text(200, 174, text="00:00", fill="white", font=("Ariel", 15, "bold"))
 canvas.grid(column=2, row=2)
 
-start_timer()
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=1, row=3)
 
+stop_button = Button(text="Stop")
+stop_button.grid(column=3, row=3)
 
-# label2 = Label(text="✔", fg='green', bg="#FFE8C5", font=("Arial", 16, "bold"))
-# label2.grid(column=2, row=4)
+label2 = Label(text="✔", fg='green', bg="#FFE8C5", font=("Arial", 16, "bold"))
+label2.grid(column=2, row=4)
 
 window.mainloop()
