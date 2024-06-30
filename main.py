dbfa25229b57ae3f6ba44706aa563d8516314c31
Amina-Sagheer <sagheeramina32@gmail.com>
2024-06-29 18:37:22 -0600

    Add reset button functionality

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from tkinter import *
 
 i = 0
-
+TIMER = None
 
 def count_down(count):
+
     if count >= 0:
         minutes = count // 60
         seconds = count % 60
         final_count = f"{minutes:02}:{seconds:02}"
         canvas.itemconfig(timer, text=final_count)
-        window.after(1000, count_down, count - 1)
+        global TIMER
+        TIMER = window.after(1000, count_down, count - 1)
+
+def reset():
+    canvas.itemconfig(timer, text="00:00")
+    window.after_cancel(TIMER)
 
 
 def start_timer():
@@ -49,8 +55,8 @@ canvas.grid(column=2, row=2)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=1, row=3)
 
-stop_button = Button(text="Stop")
-stop_button.grid(column=3, row=3)
+reset_button = Button(text="Reset", command=reset)
+reset_button.grid(column=3, row=3)
 
 label2 = Label(text="✔", fg='green', bg="#FFE8C5", font=("Arial", 16, "bold"))
 label2.grid(column=2, row=4)
